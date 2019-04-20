bd44c4f84cb3c06fcfb5ea94df371692659d8d60
meggmaier <megg.maier@gmail.com>
2019-04-20 20:15:09 +0200

    skeleton

---
+++ src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
@@ -0,0 +1,24 @@
+package com.kodilla.testing.forum.statistics;
+
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+public class ForumStatisticsTestSuite {
+
+    //Testy zrealizuj przy pomocy mocka interfejsu Statistics.
+    // Przetestuj poprawność obliczeń wartości średnich dla różnych przypadków:
+
+    //gdy liczba postów = 0,
+    //gdy liczba postów = 1000,
+    //gdy liczba komentarzy = 0,
+    //gdy liczba komentarzy < liczba postów,
+    //gdy liczba komentarzy > liczba postów,
+    //gdy liczba użytkowników = 0,
+    //gdy liczba użytkowników = 100.
+
+
+    @Test
+    void testCalculateAdvStatistics(Statistics statistics){
+
+    }
+}
