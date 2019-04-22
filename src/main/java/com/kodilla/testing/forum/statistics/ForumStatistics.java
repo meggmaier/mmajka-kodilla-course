6914226bda53b1d39d852c4bf28ac0f392ccb347
meggmaier <megg.maier@gmail.com>
2019-04-22 11:31:42 +0200

    finished

--- src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
+++ src/main/java/com/kodilla/testing/forum/statistics/ForumStatistics.java
@@ -2,20 +2,30 @@ package com.kodilla.testing.forum.statistics;
 
 public class ForumStatistics {
 
+
     public Class calculateAdvStatistics(Statistics statistics){
         int users = statistics.usersNames().size();
         int posts = statistics.postsCount();
         int comments = statistics.commentsCount();
 
-        double postToUsersAverage = posts/users;
-        double commentsToUsersAverage = comments/users;
-        double commentsToPostsAverage = comments/posts;
+        if (users != 0 && posts > users ) {
+            double postToUsersAverage = posts / users;
+        } else { double postToUsersAverage = 0;}
+
+         if (users != 0 && comments > users) {
+             double commentsToUsersAverage = comments / users;
+         } else { double commentsToUsersAverage = 0;}
+
+         if (posts != 0 && comments > posts){
+            double commentsToPostsAverage = comments / posts;
+        } else {
+            double commentsToPostsAverage = 0;
+        }
 
         return ForumStatistics.class;
     }
 
     public void showStatistics(){
-
-    }
+        System.out.println(ForumStatistics.class);    }
 
 }
