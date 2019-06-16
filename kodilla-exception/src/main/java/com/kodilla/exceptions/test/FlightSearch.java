af05b17eae7fef7ac31fb84c327b7c40adfbdf5f
meggmaier <megg.maier@gmail.com>
2019-06-16 13:15:29 +0200

    poprawione

--- kodilla-exception/src/main/java/com/kodilla/exceptions/test/FlightSearch.java
+++ kodilla-exception/src/main/java/com/kodilla/exceptions/test/FlightSearch.java
@@ -7,20 +7,32 @@ public class FlightSearch {
 
     public void findFlight(Flight flight) throws RouteNotFoundException {
 
-        Map<String,Boolean> flights = new HashMap<>();
+        String departureAirport = flight.getDepartureAirport();
+        String arrivalAirport = flight.getArrivalAirport();
+
+        Map<String, Boolean> flights = new HashMap<>();
         flights.put("Warszawa Okęcie", true);
         flights.put("Warszawa Modlin", false);
         flights.put("Wrocław", true);
         flights.put("Poznań Ławica", true);
-        flights.put("Kraków Balice",false);
+        flights.put("Kraków Balice", false);
         flights.put("Katowice Pyrzowice", true);
         flights.put("Łódź Lublinek", false);
 
-        if(flights.keySet().equals(flight.getArrivalAirport()) && flights.keySet().equals(flight.getDepartureAirport())){
-            System.out.println("You're flight is available for booking.");
-        } else {
+        if (flights.get(departureAirport) == null
+                || flights.get(arrivalAirport) == null) {
+
             throw new RouteNotFoundException();
         }
 
+        if (flights.get(departureAirport)
+            && flights.get(arrivalAirport)) {
+            System.out.println("You're flight from [" + departureAirport + "] to [" + arrivalAirport + "] is available for booking.");
+        }
+
+        else {
+            throw new RouteNotFoundException();
+        }
     }
+
 }
