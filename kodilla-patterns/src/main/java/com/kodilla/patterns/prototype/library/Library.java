6a627a95fb8c5c6446f3eaaa1d4cff3a15b1a671
meggmaier <megg.maier@gmail.com>
2019-06-26 19:13:20 +0200

    Cloning library

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -0,0 +1,38 @@
+package com.kodilla.patterns.prototype.library;
+
+import com.kodilla.patterns.prototype.Prototype;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class Library extends Prototype
+{
+    private String name;
+    private Set<Book> books = new HashSet<>();
+
+    public Library(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public Library shallowCopy() throws CloneNotSupportedException {
+        return (Library) super.clone();
+    }
+
+    public Library deepCopy() throws CloneNotSupportedException {
+        Library clonedLibrary = (Library)super.clone();
+        clonedLibrary.books = new HashSet<>();
+        for(Book theBooks : books) {
+            Book clonedBooks = new Book(theBooks.getTitle(), theBooks.getAuthor(),theBooks.getPublicationDate());
+            clonedLibrary.getBooks().add(clonedBooks);
+        }
+        return clonedLibrary;
+    }
+}
