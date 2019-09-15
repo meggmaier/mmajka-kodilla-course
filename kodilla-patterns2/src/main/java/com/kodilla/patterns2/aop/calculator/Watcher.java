c09667e3d2485777b50444811f5e01044acdbd9a
meggmaier <megg.maier@gmail.com>
2019-09-15 11:24:01 +0200

    annotations not working

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/aop/calculator/Watcher.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/aop/calculator/Watcher.java
@@ -1,7 +1,9 @@
 package com.kodilla.patterns2.aop.calculator;
 
+import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.aop.aspectj.MethodInvocationProceedingJoinPoint;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -9,7 +11,24 @@ import org.springframework.stereotype.Component;
 public class Watcher {
     private static final Logger LOGGER = LoggerFactory.getLogger((Watcher.class));
 
+    @Before("execution(* com.kodilla.patterns2.aop.calculator.Calculator.factorial(..))" +
+            "&& args(theNumber) && target(object)")
     public void logEvent() {
         LOGGER.info("Logging the event");
+    }
+    @Around("execution(* com.kodilla.patterns2.aop.calculator.Calculator.factorial(..)")
+    public Object measureTime(final MethodInvocationProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result;
+        try {
+            long begin = System.currentTimeMillis();
+            result = proceedingJoinPoint.proceed();
+            long end = System.currentTimeMillis();
+
+            LOGGER.info("Time consumed: " + (end-begin) + "[ms]");
+        } catch (Throwable throwable) {
+            LOGGER.error(throwable.getMessage());
+            throw throwable;
+        }
+        return result;
     }
 }
